9dbc2712773a3965328bd50b1d8efe682b868b1c
giampcar <36573135+giampcar@users.noreply.github.com>
2022-03-14 15:15:51 +0100

    central

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
+var myIndex = 0;
+carousel();
+carousel_1();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel,6000); // Change image every 2 seconds
+}
 
 
+function carousel_1() {
+  var i;
+  var x = document.getElementsByClassName("mySlides_2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel_1, 5000); // Change image every 2 seconds
+}
