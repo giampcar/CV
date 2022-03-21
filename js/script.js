677c3917e4f0d1a0332ea0b20b9fb00bf83395b3
giampcar <36573135+giampcar@users.noreply.github.com>
2022-03-21 11:19:37 +0100

    a

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function carousel_1() {
 
 
 var counterContainer = document.querySelector(".website-counter");
-var resetButton = document.querySelector("#reset");
+// var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
 
 // Check if page_view entry is present
@@ -45,8 +45,8 @@ if (visitCount) {
 counterContainer.innerHTML = visitCount;
 
 // Adding onClick event listener
-resetButton.addEventListener("click", () => {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-  counterContainer.innerHTML = visitCount;
-});
+// resetButton.addEventListener("click", () => {
+//   visitCount = 1;
+//   localStorage.setItem("page_view", 1);
+//   counterContainer.innerHTML = visitCount;
+// });
